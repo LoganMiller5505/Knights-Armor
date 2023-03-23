03abca768ded76e299a97e5eec38ad6c71965653
LoganMiller5505 <loganmiller5505@gmail.com>
2023-03-23 09:43:00 -0400

    Telem Stream Framework

--- Utils/TelemetryStream.java
+++ Utils/TelemetryStream.java
@@ -1,4 +1,33 @@
 package org.firstinspires.ftc.teamcode.Utils;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import java.util.ArrayList;
+
 public class TelemetryStream {
+
+    private static Telemetry telemetry;
+
+    private static ArrayList<String> telemToDisplay = new ArrayList<>();
+
+    public static void initTelemetryStream(Telemetry telemetry){
+        TelemetryStream.telemetry = telemetry;
+    }
+
+    public static void updateTelemetry(String title, String desc){
+        for(int i = 0; i<telemToDisplay.size(); i++){
+            if(telemToDisplay.get(i).equals(title)){
+                telemToDisplay.set(i+1, desc);
+                return;
+            }
+        }
+
+    }
+
+    public static void removeTelemetry(String title){
+
+    }
+
+
+
 }
