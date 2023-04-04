c7db2a8b6f2beb725129e53e51f8c2a6275f8e5c
scdrobotics <loganmiller5505@gmail.com>
2023-04-04 19:40:54 -0400

    Webhook Test
    Testing Discord webhook synchronization

--- Utils/TimerEX.java
+++ Utils/TimerEX.java
@@ -5,6 +5,10 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 //TODO: May not be necessary? We've already undertilized ElapsedTime quite a bit, so this may not even be necessary, but I will look more into ElapsedTime and see what improvements, if any, can be made since it's been so wonky to implement in general
 public class TimerEX {
 
-    ElapsedTime time;
+    ElapsedTime timer;
+
+    public TimerEX(ElapsedTime timer){
+        this.timer=timer;
+    }
 
 }
